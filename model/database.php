7424b17b5a438b71d54320ea3591d89722ee94da
Veasna Mam <danielmusau.dm19@gmail.com>
2022-04-06 17:55:45 +0300

    Sessions

--- model/database.php
+++ model/database.php
@@ -1,9 +1,10 @@
 <?php
 $dsn = 'mysql:host=wb39lt71kvkgdmw0.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=n01y8vwc04ldg7mn';
 $username = 'nldidv3lwyl10bdv';
+$password = 'jdv12tl943o05ax9';
 
 try {
-    $db = new PDO($dsn, $username);
+    $db = new PDO($dsn, $username, $password);
 }
 catch (PDOException $e) {
     $error = "Database Error: ";
